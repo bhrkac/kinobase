3a1daf29ee0ec0f85f73d165a2851aa2f6ec0013
bhrkac <bhrkac@gmail.com>
2018-05-15 23:34:23 +0200

    Change some filmReducer default state  values

--- src/reducers/filmReducer.js
+++ src/reducers/filmReducer.js
@@ -2,11 +2,17 @@ import { refCount } from "rxjs/operators";
 
 const filmReducerDefaultState = [
   {
-    id: 'tt0069865',
-    title: 'Charley Varrick',
-    year: '1973',
-    director: 'Don Siegel',
-    imdbRating: 7.5
+    id: 'tt0090605',
+    title: 'Aliens',
+    year: '1986',
+    director: 'James Cameron',
+    imdbRating: '8.4',
+    actors: 'Sigourney Weaver, Carrie Henn, Michael Biehn, Paul Reiser',
+    plot: 'Ellen Ripley is rescued by a deep salvage team after being in hypersleep for 57 years. The moon that the Nostromo visited has been colonized, but contact is lost. This time, colonial marines have impressive firepower, but will that be enough?',
+    genre: 'Action, Adventure, Sci-Fi',
+    poster: 'https://ia.media-imdb.com/images/M/MV5BYzVlMWViZGEtYjEyYy00YWZmLThmZGEtYmM4MDZlN2Q5MmRmXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+    runtime: '137 min',
+    country: 'USA'
   },
   {
     id: 'tt0052357',
@@ -23,11 +29,17 @@ const filmReducerDefaultState = [
     imdbRating: 7.8
   },
   {
-    id: 'tt0051380',
-    title: 'Attack of the 50 Foot Woman',
-    year: '1958',
-    director: 'Nathan Juran',
-    imdbRating: 5.1
+    id: 'tt0079501',
+    title: 'Mad Max',
+    year: '1979',
+    director: 'George Miller',
+    imdbRating: '7.0',
+    actors: 'Mel Gibson, Joanne Samuel, Hugh Keays-Byrne, Steve Bisley',
+    plot: 'In a self-destructing world, a vengeful Australian policeman sets out to stop a violent motorcycle gang.',
+    genre: 'Action, Adventure, Sci-Fi',
+    poster: 'https://ia.media-imdb.com/images/M/MV5BMTM4Mjg5ODEzMV5BMl5BanBnXkFtZTcwMDc3NDk0NA@@._V1_SX300.jpg',
+    runtime: '88 min',
+    country: 'Australia'
   }
 ];
 
